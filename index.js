dcdfb67679c2886ac27e4754eeaba268a68b3e9c
Hossain-A1 <mrhossainahmed7@gmail.com>
2023-12-10 23:16:08 +0300

    uri fixed

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require("express");
 require("dotenv").config();
+const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const projectsRoutes = require("./routes/projects");
@@ -26,7 +26,7 @@ app.use("/api/projects", projectsRoutes);
 app.use("/api/user", userRoutes);
 // connect mongoose
 mongoose
-  .connect(uri)
+  .connect(String(uri))
   .then(() => {
     app.listen(port, () => {
       console.log(`app listing and connected mongoDB on port ${port}`);
