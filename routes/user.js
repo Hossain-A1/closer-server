eb28dd93ab7c625fc24527d999543bfe8bb4acda
Hossain-A1 <mrhossainahmed7@gmail.com>
2023-12-10 23:22:37 +0300

    uri

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { signupUser, loginUser } = require("../controllers/user");
 
 const router = express.Router();
-
+// auth routes
 router.post("/signup", signupUser);
 router.post("/login", loginUser);
 
