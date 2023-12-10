4c29466e73949e49e5cec7983f12b6a979589144
Hossain-A1 <mrhossainahmed7@gmail.com>
2023-12-10 21:48:11 +0300

    bug fixed

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
 const express = require("express");
 require("dotenv").config();
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const cors = require("cors");
-const projectsRoutes = require("./routes/projects")
-const userRoutes = require("./routes/user")
+const projectsRoutes = require("./routes/projects");
+const userRoutes = require("./routes/user");
 
 // express app
 const app = express();
 // port
 const port = process.env.PORT || 5000;
 // uri
-const uri = process.env.MONGO_URI
+const uri = process.env.MONGODB_URI;
 // middlewares
 app.use(express.json());
 app.use(cors({ credentials: true }));
-app.use("/", (req, res,next) => {
+app.use("/", (req, res, next) => {
   console.log(req.method, req.path);
-  next()
+  next();
 });
 // routes
 app.get("/", (req, res) => {
   res.status(200).json({ message: "hello express js" });
 });
-app.use("/api/projects",projectsRoutes)
-app.use("/api/user",userRoutes)
+app.use("/api/projects", projectsRoutes);
+app.use("/api/user", userRoutes);
 // connect mongoose
-mongoose.connect(uri,{useUnifiedTopology:true}).then(()=>{
-  app.listen(port, () => {
-    console.log(`app listing and connected mongoDB on port ${port}`);
+mongoose
+  .connect(uri)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`app listing and connected mongoDB on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error.message);
   });
-})
-
